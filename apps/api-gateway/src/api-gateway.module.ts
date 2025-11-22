f642494a5557807bdf4bfa58cad9a9b96fcd8702
Wadhah998 <daoidwadhah@gmail.com>
2025-11-22 12:17:27 +0100

    Correction api-gateway.module.ts

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
-import { SharedModule } from '../../../../Libs/shared/shared.module';
+import { SharedModule } from 'Libs/shared/shared.module';
+
 
 @Module({
   imports: [
